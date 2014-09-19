153b79a75d80375d4fd62b1b5fd4a2102d30fca5
yashart <yashart@mail.ru>
2014-09-20 02:32:10 +0400

    Update quadratic_polinom.cpp
    Function is in .h file now

--- quadratic_polinom/quadratic_polinom.cpp
+++ quadratic_polinom/quadratic_polinom.cpp
@@ -1,133 +1,29 @@
 #include <stdio.h>
 #include <math.h>
 #include <locale.h>
-#include "GlobalConstants.h"
-
-#define POISON NAN
-
-
-#define POLINOM_DEGREE 2
-
-double discrim_counting(double* coefficients)
-{
-    return coefficients[1] * coefficients[1] - 4 * coefficients[0] * coefficients[2];
-}
-
-double radicals_counting(events event, double* coefficients)
-{
-    if(event == ONE_RADICAL)
-    {
-        return -coefficients[2] / coefficients[1];
-    }
-
-    if(event == TWO_RADICALS)
-    {
-        return (-coefficients[1] - sqrt(discrim_counting(coefficients)) )/ 2/coefficients[0];
-    }
-
-    if(event == TWO_RADICALS_2)
-    {
-        return (-coefficients[1] + sqrt(discrim_counting(coefficients)) )/ 2/coefficients[0];
-    }
-    return POISON;
-}
+#include "quadratic.h"
 
 
 
-void message (events event, double x1, double x2)
-{
-    if( event == ALL_NUMBERS)
-        printf( "Any number is radical");
+struct radicals;
 
-    if( event ==  NO_RADICALS)
-        printf( "No radicals");
+void radicals_destruction(struct radicals* r);
 
-    if( event == ONE_RADICAL)
-        printf( "One radical %lg", x1);
+double discrim_counting(double* coefficients);
 
-    if( event == TWO_RADICALS)
-        printf( "Two radicals %lg и %lg", x1, x2);
-}
+radicals radicals_counting(events event, double* coefficients, radicals* r);
 
-void logic(double* coefficients)
-{
-    double discrim = POISON;
+void message (events event, radicals r);
 
-    if(POLINOM_DEGREE == 2)
-    {
-        if( coefficients[0] == 0)
-        {
-            if( coefficients[1] == 0)
-            {
-                if( coefficients[2] == 0)
-                {
-                    message (ALL_NUMBERS, POISON, POISON);
-                }
-                else
-                {
-                    message (NO_RADICALS, POISON, POISON);
-                }
-            }
-            else
-            {
+void logic(double* coefficients, radicals* r);
 
-                message (ONE_RADICAL, radicals_counting(ONE_RADICAL,coefficients), POISON);
-            }
-        }
-        else
-        {
-            discrim = discrim_counting(coefficients);
-            if( discrim < 0)
-            {
-                message (NO_RADICALS, POISON, POISON);
-            }
-            else
-            {
-
-                if( discrim == 0)
-                {
-                    message (ONE_RADICAL, radicals_counting (TWO_RADICALS, coefficients ),
-                             POISON );
-                }
-                else
-                {
-                    message (TWO_RADICALS, radicals_counting (TWO_RADICALS, coefficients ),
-                             radicals_counting(TWO_RADICALS_2, coefficients ) );
-                }
-            }
-        }
-
-
-    }
-}
-
-void scan(double* coefficients)
-{
-    double right_scan = POISON;
-    for(int i = 0; i < POLINOM_DEGREE+1; i++)
-    {
-        printf( "Write the %d-st polinomal coefficients of %d polinom by a space \n",i+1 ,POLINOM_DEGREE + 1);
-
-        right_scan = scanf("%lg[0-9]", &coefficients[i]);
-        fflush(stdin);
-        while(getchar() != 10);
-        if(right_scan == 0)
-        {
-            i--;
-
-        }
-
-        right_scan = POISON;
-    }
-
-    logic(coefficients);
-
-}
+void scan(double* coefficients);
 
 int main()
 {
     double polinomal_coefficiens[POLINOM_DEGREE+1];
-
+    radicals r;
+    radicals_destruction(&r);
 
     for (int i = 0; i < POLINOM_DEGREE + 1; i++ )
     {
@@ -135,6 +31,6 @@ int main()
     }
 
     scan(polinomal_coefficiens);
+    logic(polinomal_coefficiens, &r);
     return 0;
 }
-
