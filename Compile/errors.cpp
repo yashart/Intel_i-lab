71fd9f2fbd9b41bbbe282fe50aec055158549533
yashart <yashart@mail.ru>
2014-11-09 20:52:13 +0300

    Errors witout #define :trollface:

--- Compile/errors.cpp
+++ Compile/errors.cpp
@@ -4,14 +4,7 @@
 
 void dump(const errors ERROR, const char* FUNCTION_NAME, const int LINE, const char* FILE, const bool kill)
 {
-    const char* ERROR_NAME = "0";
-    #define errorsdata(name,value)\
-     if(value == ERROR){\
-        ERROR_NAME = #name;\
-     }
-    #include "errors_data.h"
-    #undef errorsdata
-    printf("ERROR!!! %s in %s file in %s line: %d", ERROR_NAME, FILE, FUNCTION_NAME, LINE);
+    printf("ERROR!!! %d in %s file in %s line: %d", ERROR, FILE, FUNCTION_NAME, LINE);
     if(kill)
         assert(0);
 }
